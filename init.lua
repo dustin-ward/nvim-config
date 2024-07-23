d3649e0188bd96902fa3ca9354a53832bda559f1
dustin-ward <wardddustin@gmail.com>
2024-07-23 19:45:52 -0400

    Redo nvim config 2024/07

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
-require 'dustinw.plugins'
-require 'dustinw.settings'
-require 'dustinw.maps'
+require("dustinw.lazy")
+require("dustinw.set")
+require("dustinw.remap")
 
-local themeStatus, kanagawa = pcall(require, "kanagawa")
-
-if themeStatus then
-	vim.cmd("colorscheme kanagawa")
-else
-	return
-end
+FixColours()
