25270193390f31da392de1cd050f418141378876
dustin-ward <wardddustin@gmail.com>
2024-12-10 16:59:27 -0500

    Add darkvoid theme

--- lua/dustinw/lazy/themery.lua
+++ lua/dustinw/lazy/themery.lua
@@ -12,6 +12,7 @@ return {
             "eldritch",
             "onedark",
             "lackluster",
+            "darkvoid",
         },                -- Your list of installed colorschemes.
         livePreview = true, -- Apply theme while picking. Default to true.
     }
